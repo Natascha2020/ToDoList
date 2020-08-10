728d0fad38e534165a5a93f1601ee9a774bf0d1d
Natascha <NataschaHof@gmx.de>
2020-08-10 16:37:50 +0200

    Update Bootstrap and DivWrapper

--- index.js
+++ index.js
@@ -6,32 +6,41 @@ const editButton = document.getElementsByClassName("edit-todo");
 const deleteButton = document.getElementsByClassName("delete-todo");
 
 
+//Add ToDo-Item with Text and Button
+
 addButton.addEventListener('click', (event) =>{
     event.preventDefault();
+
+    let liWrapper = document.createElement('div');
+    liWrapper.className += "li-wrapper d-flex";
+    boxUl.appendChild(liWrapper);
+    
     let inputText = input.value;
     let newLi = document.createElement('li');
     let newLiText = document.createTextNode(inputText);
 
-    boxUl.appendChild(newLi);
+    liWrapper.appendChild(newLi);
+    newLi.className += "form-control";
     let newLiWhole = newLi.appendChild(newLiText); //new li with text
 
     let editButtonNew=document.createElement('button');
-    editButtonNew.className += "edit-todo";
+    editButtonNew.className += "edit-todo btn btn-primary";
     editButtonNewText = document.createTextNode('Edit');
     editButtonNew.appendChild(editButtonNewText);
-    boxUl.appendChild(editButtonNew);
+    liWrapper.appendChild(editButtonNew);
 
     let deleteButtonNew=document.createElement('button');
-    deleteButtonNew.className += "delete-todo";
+    deleteButtonNew.className += "delete-todo btn btn-danger";
     deleteButtonNewText = document.createTextNode('Delete');
     deleteButtonNew.appendChild(deleteButtonNewText);
+    liWrapper.appendChild(deleteButtonNew);
 
-    boxUl.appendChild(deleteButtonNew);
+    newLiWhole.style.marginRight = "4px"; //space between new Buttons
 
-
-
-    console.log(newLi);
+    /* console.log(newLi);
     console.log(newLiText);
     console.log(editButtonNew);
-    console.log(deleteButtonNew);
+    console.log(deleteButtonNew); */
 })
+
+//End
