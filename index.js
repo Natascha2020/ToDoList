0cceb9877fca1c463ff7fc33db76de0950be6b33
simo54 <sorzesimone1@gmail.com>
2020-08-10 11:50:50 +0200

    updating js file with consts

---
+++ index.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("new-item");
+const addButton = document.getElementById("add-todo");
+const boxUl = document.getElementById("list-todo");
+const toDo = document.getElementsByTagName("li"); // here we select ALL li items
+const editButton = document.getElementsByClassName("edit-todo");
+const deleteButton = document.getElementsByClassName("delete-todo");
