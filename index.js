b017e508ea3f9b9cf3a119d28bb77e31de53f073
simo54 <sorzesimone1@gmail.com>
2020-08-10 16:37:47 +0200

    updated with deletefunction

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const boxUl = document.getElementById("list-todo");
 const toDo = document.getElementsByTagName("li"); // here we select ALL li items
 const editButton = document.getElementsByClassName("edit-todo");
 const deleteButton = document.getElementsByClassName("delete-todo");
+
+
+// functions Edit
+
+
+
+// functions Delete
+
+Object.keys(deleteButton).forEach(function (key){
+    addEventListener('click', (e) => {
+        let remove = e.target.parentElement;
+        boxUl.removeChild(remove);
+    })
+})
+
+
