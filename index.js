e649f5875b862ab235003e485e8ba9195511b6ca
Natascha <NataschaHof@gmx.de>
2020-08-11 17:54:56 +0200

    small structure adjustments

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const toDo = document.getElementsByTagName("li"); // here we select ALL li items
 const editButton = document.getElementsByClassName("edit-todo");
 const deleteButton = document.getElementsByClassName("delete-todo");
 
+// Add Input and Buttons
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let liWrapper = document.createElement("div");
+  liWrapper.className += "li-wrapper d-flex";
+  boxUl.appendChild(liWrapper);
+
+  let inputText = input.value;
+  let newLi = document.createElement("li");
+  let newLiText = document.createTextNode(inputText);
+
+  liWrapper.appendChild(newLi);
+  newLi.className += "form-control";
+  newLi.setAttribute("contenteditable", "true");
+  let newLiWhole = newLi.appendChild(newLiText); //new li with text
+
+  let editButtonNew = document.createElement("button");
+  editButtonNew.className += "edit-todo btn btn-primary";
+  editButtonNew.setAttribute("type", "button");
+  editButtonNewText = document.createTextNode("Edit");
+  editButtonNew.appendChild(editButtonNewText);
+  liWrapper.appendChild(editButtonNew);
+
+  let deleteButtonNew = document.createElement("button");
+  deleteButtonNew.className += "delete-todo btn btn-danger";
+  deleteButtonNew.setAttribute("type", "button");
+  deleteButtonNewText = document.createTextNode("Delete");
+  deleteButtonNew.appendChild(deleteButtonNewText);
+  liWrapper.appendChild(deleteButtonNew);
+});
+
 // fuction Edit
 
 Object.keys(editButton).forEach((key) => {
@@ -31,7 +64,7 @@ Object.keys(editButton).forEach((key) => {
 
 // functions Delete
 
-Object.keys(deleteButton).forEach((key) => {
+Object.keys(editButton).forEach((key) => {
   deleteButton[key].addEventListener("click", (e) => {
     if (e.target.classList.contains("delete-todo")) {
       if (confirm("Sure you are done?")) {
@@ -41,40 +74,3 @@ Object.keys(deleteButton).forEach((key) => {
     }
   });
 });
-
-// Add Input and Buttons
-
-addButton.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  let liWrapper = document.createElement("div");
-  liWrapper.className += "li-wrapper d-flex";
-  boxUl.appendChild(liWrapper);
-
-  let inputText = input.value;
-  let newLi = document.createElement("li");
-  let newLiText = document.createTextNode(inputText);
-
-  liWrapper.appendChild(newLi);
-  newLi.className += "form-control";
-  let newLiWhole = newLi.appendChild(newLiText); //new li with text
-
-  let editButtonNew = document.createElement("button");
-  editButtonNew.className += "edit-todo btn btn-primary";
-  editButtonNewText = document.createTextNode("Edit");
-  editButtonNew.appendChild(editButtonNewText);
-  liWrapper.appendChild(editButtonNew);
-
-  let deleteButtonNew = document.createElement("button");
-  deleteButtonNew.className += "delete-todo btn btn-danger";
-  deleteButtonNewText = document.createTextNode("Delete");
-  deleteButtonNew.appendChild(deleteButtonNewText);
-  liWrapper.appendChild(deleteButtonNew);
-
-  /* console.log(newLi);
-    console.log(newLiText);
-    console.log(editButtonNew);
-    console.log(deleteButtonNew); */
-});
-
-//End
