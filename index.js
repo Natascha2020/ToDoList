ed78e35061876b4593cda927b0945643face3e4e
simo54 <sorzesimone1@gmail.com>
2020-08-12 12:51:33 +0200

    Comments on js file

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
+// This js file is structured in the following ways:
+
+//   - Constant Declaration: declarations of most importants elements.
+//   - Add function: function that insert a new toDo element, edit and delete buttons + edit and delete
+//     function for new items added to the list
+//   - Edit function: function that modify an existing toDo by pressing the edit button
+//   - Delete function: function that deletes an existing toDo by pressing the delete button
+//
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+// Constant Declaration
+
 const input = document.getElementById("new-item");
 const addButton = document.getElementById("add-todo");
 const boxUl = document.getElementById("list-todo");
-const toDo = document.getElementsByTagName("li"); // here we select ALL li items
+const toDo = document.getElementsByTagName("li");
 const editButton = document.getElementsByClassName("edit-todo");
 const deleteButton = document.getElementsByClassName("delete-todo");
 
-// Add Input and Buttons
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+// - Add function -
 
 addButton.addEventListener("click", (event) => {
   event.preventDefault();
 
+  // Create div elements
   let liWrapper = document.createElement("div");
   liWrapper.className += "li-wrapper d-flex";
   boxUl.appendChild(liWrapper);
 
+  // Create li elements
   let inputText = input.value;
   let newLi = document.createElement("li");
   let newLiText = document.createTextNode(inputText);
 
+  // Assign a class name to new li elements
   liWrapper.appendChild(newLi);
   newLi.className += "form-control";
-  let newLiWhole = newLi.appendChild(newLiText); //new li with text
+  let newLiWhole = newLi.appendChild(newLiText);
 
+  // New edit button
   let editButtonNew = document.createElement("button");
   editButtonNew.className += "edit-todo btn btn-primary";
   editButtonNew.setAttribute("type", "button");
   editButtonNewText = document.createTextNode("Edit");
   editButtonNew.appendChild(editButtonNewText);
-  liWrapper.appendChild(editButtonNew);
+  liWrapper.appendChild(editButtonNew); // new edit button
 
+  // New delete button
   let deleteButtonNew = document.createElement("button");
   deleteButtonNew.className += "delete-todo btn btn-danger";
   deleteButtonNew.setAttribute("type", "button");
@@ -36,6 +56,8 @@ addButton.addEventListener("click", (event) => {
   deleteButtonNew.appendChild(deleteButtonNewText);
   liWrapper.appendChild(deleteButtonNew);
 
+  // - Delete function addressing new added items -
+  // This alert user about removing new items after clicking on delete button
   deleteButtonNew.addEventListener("click", (e) => {
     if (confirm("Sure you are done?")) {
       let remove = e.target.parentElement;
@@ -43,6 +65,8 @@ addButton.addEventListener("click", (event) => {
     }
   });
 
+  //  - Edit function addressing new added items -
+  // This make li editable once clicked on edit button and focus the li tag
   editButtonNew.addEventListener("click", (e) => {
     let newText = e.target.parentElement.firstChild;
     newText.contentEditable = true;
@@ -50,14 +74,17 @@ addButton.addEventListener("click", (event) => {
     console.log(newText);
   });
 
+  // This prevent text changes of the edit button itself once clicked in
   editButtonNew.addEventListener("dblclick", (e) => {
     let newText = e.target.parentElement.firstChild;
     newText.contentEditable = false;
   });
 });
 
-// fuction Edit
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
+// - Edit function -
+// This make existing li tags editable once clicked on edit button, also focus the li tag
 Object.keys(editButton).forEach((key) => {
   editButton[key].addEventListener("click", (e) => {
     if (e.target.classList.contains("edit-todo")) {
@@ -71,6 +98,7 @@ Object.keys(editButton).forEach((key) => {
   });
 });
 
+// This prevent text changes of the edit button itself once clicked in
 Object.keys(editButton).forEach((key) => {
   editButton[key].addEventListener("dblclick", (e) => {
     if (e.target.classList.contains("edit-todo")) {
@@ -80,8 +108,10 @@ Object.keys(editButton).forEach((key) => {
   });
 });
 
-// functions Delete
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
+// - Delete function -
+// This alert user about removing items after clicking on delete button
 Object.keys(deleteButton).forEach((key) => {
   deleteButton[key].addEventListener("click", (e) => {
     if (e.target.classList.contains("delete-todo")) {
@@ -92,3 +122,5 @@ Object.keys(deleteButton).forEach((key) => {
     }
   });
 });
+
+// End of the index.js file
